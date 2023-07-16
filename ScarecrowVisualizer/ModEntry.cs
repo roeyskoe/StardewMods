2e0411e7fb5ec5deb91ae674a056e88f553d1ac6
Mikko <mikko.roysko@gmail.com>
2023-07-16 20:11:43 +0300

    Show on mousehover & ease in visibility

--- ScarecrowVisualizer/ModEntry.cs
+++ ScarecrowVisualizer/ModEntry.cs
@@ -3,6 +3,8 @@ using StardewModdingAPI;
 using StardewValley;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
+using System.Diagnostics;
+using System;
 
 namespace FasterToolUse
 {
@@ -16,26 +18,65 @@ namespace FasterToolUse
             helper.Events.Display.RenderedWorld += RenderedWorld;
         }
 
+        float hovertime = 0;
+
         private void RenderedWorld(object sender, RenderedWorldEventArgs e)
         {
-            if (Game1.player != null && (Game1.player.ActiveObject?.IsScarecrow() ?? false))
+            if (Game1.player != null)
             {
-                foreach (var obj in Game1.player.currentLocation?.Objects.Values)
+                if ((Game1.player.ActiveObject?.IsScarecrow() ?? false) || IsHoveringOverScarecrow())
                 {
-                    if (obj.IsScarecrow())
+                    hovertime += 0.02f;
+                    hovertime = MathHelper.Clamp(hovertime, 0, 1);
+                   
+                }
+                else
+                {
+                    hovertime -= 0.02f;
+                    hovertime = MathHelper.Clamp(hovertime, 0, 1);
+                }
+                if(hovertime > 0.01)
+                {
+                    RenderRanges(e.SpriteBatch);
+                }
+            }
+        }
+
+        public bool IsHoveringOverScarecrow()
+        {
+            ICursorPosition cursorPos = Helper.Input.GetCursorPosition();
+            var pos = cursorPos.Tile;
+            var obj = Game1.player.currentLocation?.getObjectAtTile((int)pos.X, (int)pos.Y);
+
+            if (this.Helper.Input.IsDown(SButton.LeftShift))
+                Debugger.Break();
+
+            return obj?.IsScarecrow() ?? false;
+        }
+
+        // https://easings.net/#easeInQuart
+        private float Ease(float x)
+        {
+            return x * x * x * x;
+        }
+
+        private void RenderRanges(SpriteBatch batch)
+        {
+            foreach (var obj in Game1.player.currentLocation?.Objects.Values)
+            {
+                if (obj.IsScarecrow())
+                {
+                    var radius = obj.GetRadiusForScarecrow();
+                    var v = obj.TileLocation;
+                    for (var i = -radius; i < radius; i++)
                     {
-                        var radius = obj.GetRadiusForScarecrow();
-                        var v = obj.TileLocation;
-                        for (var i = -radius; i < radius; i++)
+                        for (int j = -radius; j < radius; j++)
                         {
-                            for (int j = -radius; j < radius; j++)
+                            var tilePos = new Vector2(v.X + i, v.Y + j);
+                            if (Vector2.Distance(tilePos, v) < radius)
                             {
-                                var tilePos = new Vector2(v.X + i, v.Y + j);
-                                if (Vector2.Distance(tilePos, v) < radius)
-                                {
-                                    var drawPos = Game1.GlobalToLocal(new Vector2(tilePos.X * 64, tilePos.Y * 64));
-                                    e.SpriteBatch.Draw(Game1.mouseCursors, drawPos, new Rectangle(194, 388, 16, 16), Color.White*1f, 0f, Vector2.Zero, 4f, SpriteEffects.None, 0.01f);
-                                }   
+                                var drawPos = Game1.GlobalToLocal(new Vector2(tilePos.X * 64, tilePos.Y * 64));
+                                batch.Draw(Game1.mouseCursors, drawPos, new Rectangle(194, 388, 16, 16), Color.White * Ease(hovertime), 0f, Vector2.Zero, 4f, SpriteEffects.None, 0.01f);
                             }
                         }
                     }
